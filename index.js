ae6fb08741edafb9cd3b6a1bbbae0d2b9244a322
Mehmet Faahem <mehmetfaahem@gmail.com>
2022-11-25 22:19:01 +0600

    main

--- index.js
+++ index.js
@@ -1,12 +1,15 @@
 const express = require("express");
 const app = express();
-app.use(function (req, res, next) {
-    res.setHeader('Access-Control-Allow-Origin', '*');
-    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
-    res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
-    res.setHeader('Access-Control-Allow-Credentials', true);
-    next();
-});
+const http = require('http').Server(app)
+
+// app.use(function (req, res, next) {
+//     res.setHeader('Access-Control-Allow-Origin', '*');
+//     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+//     res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
+//     res.setHeader('Access-Control-Allow-Credentials', true);
+//     next();
+// });
+
 const cors = require('cors')
 app.use(cors())
 const mongoose = require('mongoose')
@@ -14,6 +17,11 @@ const bodyParser = require('body-parser');
 require('dotenv').config();
 const port = process.env.PORT || 5000;
 const mongoString = process.env.DATABASE_URL
+
+const fs = require('fs')
+const messageData = fs.readFileSync('messages.json')
+
+const messagesData = JSON.parse(messageData)
 
 const routes = require('./routes/routes')
 const passwordReset = require('./routes/password-reset')
@@ -25,6 +33,15 @@ app.use(bodyParser.json())
 
 
 app.use(express.json());
+
+const socketIO = require('socket.io')(http, {
+
+    cors: {
+        origin: "http://localhost:3000/"
+    }
+
+});
+
 app.use('/api', routes)
 app.use('/api/category', categoryroute)
 app.use('/api/product', productroute)
@@ -40,6 +57,46 @@ database.on('error', (error) => {
 database.once('connected', () => {
     console.log('Database Connected');
 })
+
+let users = [];
+
+socketIO.on('connection', (socket) => {
+    console.log(`⚡: ${socket.id} user just connected!`);
+
+    socket.on('friendID', (data) => {
+        socketIO.emit('clickedID', data);
+    })
+
+    socket.on("message", data => {
+        messagesData["messages"].push(data)
+        const stringData = JSON.stringify(messagesData, null, 2)
+        fs.writeFile("messages.json", stringData, (err) => {
+            console.error(err)
+        })
+        socketIO.emit("messageResponse", data)
+    })
+
+
+
+    socket.on('joining-room', room => {
+        socket.join(room)
+    })
+
+
+    socket.on('typing', (data) => socket.broadcast.emit('typingResponse', data));
+
+    socket.on('newUser', (data) => {
+        users.push(data);
+        socketIO.emit('newUserResponse', users);
+    });
+
+    socket.on('disconnect', () => {
+        console.log('🔥: A user disconnected');
+        users = users.filter((user) => user.socketID !== socket.id);
+        socketIO.emit('newUserResponse', users);
+        socket.disconnect();
+    });
+});
 
 
 app.get("/", (req, res) => {
